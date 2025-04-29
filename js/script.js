100712acf3807090fb1c3c0e23340af4c0fae361
Your Name <you@example.com>
2025-04-29 07:36:35 -0500

    Nueva version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("¡Bienvenido a mi portafolio!");
+  console.log("¡Bienvenido al portafolio de Alonso!");
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+      card.style.boxShadow = "0 6px 20px rgba(0, 0, 0, 0.2)";
+    });
+    card.addEventListener("mouseleave", () => {
+      card.style.boxShadow = "0 4px 20px rgba(0, 0, 0, 0.08)";
+    });
   });
-  
+});
